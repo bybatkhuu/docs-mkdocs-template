d82d53d5c850a77f7abbf768043be18084d3a88b
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-12-01 18:49:21 +0900

    scripts: update scripts to use consistent shebang and improve error messages

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,28 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
 
 ## --- Base --- ##
-# Getting path of this script file:
-_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+_SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-"$0"}")" >/dev/null 2>&1 && pwd -P)"
 _PROJECT_DIR="$(cd "${_SCRIPT_DIR}/.." >/dev/null 2>&1 && pwd)"
 cd "${_PROJECT_DIR}" || exit 2
 
 
-# Loading .env file (if exists):
-if [ -f ".env" ]; then
-	# shellcheck disable=SC1091
-	source .env
-fi
-
-
-if [ -z "$(which mkdocs)" ]; then
-	echo "[ERROR]: 'mkdocs' not found or not installed!"
+if ! command -v mkdocs >/dev/null 2>&1; then
+	echo "[ERROR]: Not found 'mkdocs' command, please install it first!" >&2
 	exit 1
 fi
 
-# if [ -z "$(which mike)" ]; then
-# 	echo "[ERROR]: 'mike' not found or not installed!"
+# if ! command -v mike >/dev/null 2>&1; then
+# 	echo "[ERROR]: Not found 'mike' command, please install it first!" >&2
 # 	exit 1
 # fi
 ## --- Base --- ##
